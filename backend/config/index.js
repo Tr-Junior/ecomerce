bde3e1616a7336394e236172d3ed727fb6d7a866
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-06-06 11:55:02 -0300

    adicionado index.js

---
+++ backend/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: process.env.NODE_ENV !== 'production' ? process.env.SECRET : 'askl;djf56)(#$sd4fasd 564a@#*($&@#(*&$sdf a6s54dfs@#*$&df68 %&@#(*&#@$SDSDFF',
+    api: process.env.NODE_ENV !== 'production' ? '' : 'http://localhost:3000',
+    loja: process.env.NODE_ENV !== 'production' ? '' : 'http://localhost:8000',
+
+}
