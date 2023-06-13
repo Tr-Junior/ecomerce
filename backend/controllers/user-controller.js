ec2568bb9d3690f072fc1a8a9c42a90a02083c52
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-06-13 16:09:41 -0300

    ajustes e correções

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,35 +1,36 @@
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const sendMailRecovery = require('../helpers/email-recovery');
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const sendMailRecovery = require("../helpers/email-recovery");
 
 class UserController {
-
     //GET
 
     index(req, res, next) {
-        User.findById(req.payload.id).then(user => {
-            if (!user) return res.status(401).json({ errors: 'Usuário não registrado' });
+        User.findById(req.payload.id).then((user) => {
+            if (!user)
+                return res.status(401).json({ errors: "Usuário não registrado" });
             return res.json({ user: user.sendAuthJSON() });
-        })
+        });
     }
 
     //GET /:id
 
     show(req, res, next) {
         User.findById(req.params.id)
-            //.populate({ path: 'store' })
-            .then(user => {
-                if (!user) return res.status(401).json({ errors: 'Usuário não registrado' });
+            .populate({ path: "store" })
+            .then((user) => {
+                if (!user)
+                    return res.status(401).json({ errors: "Usuário não registrado" });
                 return res.json({
                     user: {
                         name: user.name,
                         email: user.email,
                         permission: user.permission,
-                        store: user.store
-
-                    }
+                        store: user.store,
+                    },
                 });
-            }).catch(next);
+            })
+            .catch(next);
     }
 
     //POST /register
@@ -37,12 +38,16 @@ class UserController {
     store(req, res, next) {
         const { name, email, password, store } = req.body;
 
-        if (!name || !email || !password || !store) return res.status(422).json({ errors: 'Preencha todos os campos de cadastro.' })
+        if (!name || !email || !password || !store)
+            return res
+                .status(422)
+                .json({ errors: "Preencha todos os campos de cadastro." });
 
         const user = new User({ name, email, store });
         user.setPassword(password);
 
-        user.save()
+        user
+            .save()
             .then(() => res.json({ user: user.sendAuthJSON() }))
             .catch((err) => {
                 console.log(err);
@@ -54,40 +59,57 @@ class UserController {
 
     update(req, res, next) {
         const { name, email, password } = req.body;
-        User.findById(req.payload.id).then((user) => {
-            if (!user) return res.status(401).json({ errors: 'Usuário não registrado' });
-            if (typeof name !== 'undefined') user.name = name;
-            if (typeof email !== 'undefined') user.email = email;
-            if (typeof password !== 'undefined') user.password = password;
+        User.findById(req.payload.id)
+            .then((user) => {
+                if (!user)
+                    return res.status(401).json({ errors: "Usuário não registrado" });
+                if (typeof name !== "undefined") user.name = name;
+                if (typeof email !== "undefined") user.email = email;
+                if (typeof password !== "undefined") user.password = password;
 
-            return user.save().then(() => {
-                return res.json({ user: user.sendAuthJSON() });
-            }).catch(next);
-        }).catch(next);
+                return user
+                    .save()
+                    .then(() => {
+                        return res.json({ user: user.sendAuthJSON() });
+                    })
+                    .catch(next);
+            })
+            .catch(next);
     }
 
     //DELETE /
     remove(req, res, next) {
-        User.findById(req.payload.id).then(user => {
-            if (!user) return res.status(401).json({ errors: 'Usuário não registrado' });
-            return user.remove().then(() => {
+        User.findByIdAndRemove(req.payload.id)
+            .then((user) => {
+                if (!user) {
+                    return res.status(401).json({ errors: "Usuário não registrado" });
+                }
                 return res.json({ delete: true });
-            }).catch(next);
-        }).catch(next);
+            })
+            .catch(next);
     }
-
 
     //POST /login
 
     login(req, res, next) {
         const { email, password } = req.body;
-        if (!email) return res.status(422).json({ errors: { email: 'Não pode ficar vazio' } });
-        if (!password) return res.status(422).json({ errors: { password: 'Não pode ficar vazio' } });
-        User.findOne({ email }).then((user) => {
-            if (!user) return res.status(401).json({ errors: 'Usuário não registrado' });
-            if (!user.validatorPassword(password)) return res.status(401).json({ errors: 'Senha invalida' });
-            return res.json({ user: user.sendAuthJSON() });
-        }).catch(next);
+        if (!email)
+            return res
+                .status(422)
+                .json({ errors: { email: "Não pode ficar vazio" } });
+        if (!password)
+            return res
+                .status(422)
+                .json({ errors: { password: "Não pode ficar vazio" } });
+        User.findOne({ email })
+            .then((user) => {
+                if (!user)
+                    return res.status(401).json({ errors: "Usuário não registrado" });
+                if (!user.validatorPassword(password))
+                    return res.status(401).json({ errors: "Senha invalida" });
+                return res.json({ user: user.sendAuthJSON() });
+            })
+            .catch(next);
     }
 
     //RECOVERY PASSWORD
@@ -95,52 +117,99 @@ class UserController {
     //GET /recovery-password
 
     showRecovery(req, res, next) {
-        return res.render('recovery', { error: null, success: null });
+        return res.render("recovery", { error: null, success: null });
     }
 
     //POST /recovery-password
     createRecovery(req, res, next) {
         const { email } = req.body;
-        if (!email) return res.render('recovery', { error: "Preencha com o seu email", success: null });
+        if (!email) {
+            return res.render("recovery", {
+                error: "Preencha com o seu email",
+                success: null,
+            });
+        }
 
-        User.findOne({ email }).then((user) => {
-            if (!user) return res.render("recovery", { error: "Não existe usuário com este email", success: null });
-            const recoveryData = user.criarTokenRecuperacaoSenha();
-            return user.save().then(() => {
-                enviarEmailRecovery({ user, recovery: recoveryData }, (error = null, success = null) => {
-                    return res.render("recovery", { error, success });
-                });
-            }).catch(next);
-        }).catch(next);
+        User.findOne({ email })
+            .then((user) => {
+                if (!user) {
+                    return res.render("recovery", {
+                        error: "Não existe usuário com este email",
+                        success: null,
+                    });
+                }
+
+                const recoveryData = user.createTokenRecoveryPassword();
+                user.save()
+                    .then(() => {
+                        sendMail({ user, recovery: recoveryData }, (error, success) => {
+                            return res.render("recovery", { error, success });
+                        });
+                    })
+                    .catch(next);
+            })
+            .catch(next);
     }
+
 
     //GET /recovered-password
 
     showCompletedRecovery(req, res, next) {
-        if (!req.query.token) return res.render('recovery', { error: 'Token não identificado', success: null });
-        User.findOne({ 'recovery.token': req.query.token }).then(user => {
-            if (!user) return res.render('recovery', { error: 'Não existe usuário com este token', success: null });
-            if (new Date(user.recovery.date) < new Date()) return res.render('recovery', { error: 'token expirado. Tente novamente.', success: null });
-            return res.render('recovery/store', { error: null, success: null, token: req.query.token });
-        }).catch(next);
+        if (!req.query.token)
+            return res.render("recovery", {
+                error: "Token não identificado",
+                success: null,
+            });
+        User.findOne({ "recovery.token": req.query.token })
+            .then((user) => {
+                if (!user)
+                    return res.render("recovery", {
+                        error: "Não existe usuário com este token",
+                        success: null,
+                    });
+                if (new Date(user.recovery.date) < new Date())
+                    return res.render("recovery", {
+                        error: "token expirado. Tente novamente.",
+                        success: null,
+                    });
+                return res.render("recovery/store", {
+                    error: null,
+                    success: null,
+                    token: req.query.token,
+                });
+            })
+            .catch(next);
     }
 
     //POST /recovered-password
     completeRecovery(req, res, next) {
         const { token, password } = req.body;
-        if (!token || !password) return res.render('recovery/store', { error: 'preencha novamente com sua nova senha', success: null, token: token });
-        User.findOne({ 'recovery.token': token }).then(user => {
-            if (!user) return res.render('recovery', { error: 'Usuário não identificado', success: null });
+        if (!token || !password)
+            return res.render("recovery/store", {
+                error: "preencha novamente com sua nova senha",
+                success: null,
+                token: token,
+            });
+        User.findOne({ "recovery.token": token }).then((user) => {
+            if (!user)
+                return res.render("recovery", {
+                    error: "Usuário não identificado",
+                    success: null,
+                });
 
             user.finalizeTokenRecoveryPassword();
             user.setPassword(password);
-            return user.save().then(() => {
-                return res.render('recovery/store', {
-                    error: null,
-                    success: 'Senha alterada com sucesso. Tente fazer o login novamente.',
-                    token: null
+            return user
+                .save()
+                .then(() => {
+                    return res.render("recovery/store", {
+                        error: null,
+                        success:
+                            "Senha alterada com sucesso. Tente fazer o login novamente.",
+                        token: null,
+                    });
                 })
-            }).catch(next);
+                .catch(next);
         });
     }
 }
--- backend/helpers/email-recovery.js
+++ backend/helpers/email-recovery.js
@@ -1,24 +1,23 @@
 const transporter = require('nodemailer').createTransport(require('../config/email'));
-const { root: link } = require('../config/index');
-
+const { api: link } = require('../config/index');
 
 module.exports = ({ user, recovery }, cb) => {
-    const messase = `
-    <h1 style="text-align: center;">Recuperacao de Senha</h1>
+    const message = `
+    <h1 style="text-align: center;">Recuperação de Senha</h1>
     <br />
     <p>
-        Aqui está o link para redefinir a sua senha. Acesse ele e digite sua nova senha:
+        Aqui está o link para redefinir sua senha. Acesse-o e digite sua nova senha:
     </p>
-    <a href="${link}/v1/api/usuarios/senha-recuperada?token=${recovery.token}">
-        ${link}/v1/api/usuarios/senha-recuperada?token=${recovery.token}
+    <a href="${link}/v1/api/user/recovered-password?token=${recovery.token}">
+        ${link}/v1/api/user/recovered-password?token=${recovery.token}
     </a>
     <br /><br /><hr />
     <p>
-        Obs.: Se você não solicitou a redefinicao, apenas ignore esse email.
+        Obs.: Se você não solicitou a redefinição, apenas ignore este email.
     </p>
     <br />
     <p>Atenciosamente, Loja TI</p>
-`;
+  `;
 
     const optionEmail = {
         from: 'tarcisioosicrat@gmail.com',
@@ -33,11 +32,11 @@ module.exports = ({ user, recovery }, cb) => {
                 console.log(error);
                 return cb('Aconteceu um erro no envio do email, tente novamente.');
             } else {
-                return cb(null, 'Link para redefinição de senha enviado com sucesso para o seu email');
+                return cb(null, 'Link para redefinição de senha enviado com sucesso para o seu email.');
             }
         });
     } else {
         console.log(optionEmail);
-        return cb(null, 'Link para redefinição de senha enviado com sucesso para o seu email');
+        return cb(null, 'Link para redefinição de senha enviado com sucesso para o seu email.');
     }
-}
+};
