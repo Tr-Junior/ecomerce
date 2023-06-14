8e7f35b8bb477bedc56b9588ae65137e9eddcc56
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-06-14 13:03:12 -0300

    adicionado store.js

---
+++ backend/models/store.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const StoreSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    cnpj: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: [{ type: String }]
+    },
+    address: {
+        type: {
+            locale: {
+                type: String,
+                required: true
+            },
+            number: {
+                type: String,
+                required: true
+            },
+            complement: {
+                type: String
+            },
+            neighborhood: {
+                type: String,
+                required: true,
+            },
+            city: {
+                type: String,
+                required: true,
+            },
+            zipCode: {
+                type: String,
+                required: true,
+            },
+            required: true
+        }
+    }
+}, { timestamps: true })
+
+StoreSchema.plugin(uniqueValidator, { message: 'já está sendo utilizado' });
+
+
+module.exports = mongoose.model('Store', StoreSchema);
