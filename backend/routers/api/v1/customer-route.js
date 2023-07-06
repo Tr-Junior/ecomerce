1539672a77a06fa829a01fe1f6e5c2f6dcb0729b
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-07-06 16:51:16 -0300

    ajuste nas rotas customer

--- backend/routers/api/v1/customer-route.js
+++ backend/routers/api/v1/customer-route.js
@@ -2,29 +2,30 @@ const router = require('express').Router();
 
 const auth = require('../../auth');
 
+const CustomerController = require("../../../controllers/customer-controller");
+
 const { StoreValidator } = require('../../../controllers/validator/store-validator');
-const { UserValidation } = require('../../../controllers/validator/user-Validator');
+const { CustomerValidator } = require('../../../controllers/validator/customer-validator');
 
 const { validate } = require('express-validation');
 
-
-const customerController = new customerController();
+const customerController = new CustomerController();
 
 //ADMIN
-router.get('/', auth.required, StoreValidator.admin, customerController.index);
-router.get('/search/:search/orders', auth.required, StoreValidator.admin, customerController.searchOrders);
-router.get('/search/:search', auth.required, StoreValidator.admin, customerController.search);
+router.get('/', auth.required, StoreValidator.admin, validate(CustomerValidator.index), customerController.index);
+// router.get('/search/:search/orders', auth.required, StoreValidator.admin, customerController.searchOrders);
+router.get('/search/:search', auth.required, validate(CustomerValidator.search), StoreValidator.admin, customerController.search);
 
-router.get('/admin/:id', auth.required, StoreValidator.admin, customerController.showAdmin);
-router.get('/admin/:id/:orders', auth.required, StoreValidator.admin, customerController.showOrdersCustomers);
+router.get('/admin/:id', auth.required, StoreValidator.admin, validate(CustomerValidator.showAdmin), customerController.showAdmin);
+// router.get('/admin/:id/:orders', auth.required, StoreValidator.admin, customerController.showOrdersCustomers);
 
-router.put("/admin/id:", auth.required, StoreValidator.admin, customerController.updateAdmin);
+router.put('/admin/:id', auth.required, StoreValidator.admin, validate(CustomerValidator.updateAdmin), customerController.updateAdmin);
 
 //CUSTOMER
-router.get("/:id", auth.required, customerController.show);
+router.get("/:id", auth.required, validate(CustomerValidator.show), customerController.show);
 
-router.post("/", customerController.store);
-router.put("/:id", auth.required, customerController.update);
+router.post("/", validate(CustomerValidator.store), customerController.store);
+router.put("/:id", auth.required, validate(CustomerValidator.update), customerController.update);
 router.delete("/:id", auth.required, customerController.remove);
 
 module.exports = router;
