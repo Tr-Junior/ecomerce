c193df59c2cfca294593ae0457b52a3b4d59f2e2
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-07-06 16:51:39 -0300

    adicionado a rota customer

--- backend/routers/api/v1/index.js
+++ backend/routers/api/v1/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/user', require('./users-route'));
 router.use('/store', require('./store-route'));
+router.use('/customer', require('./customer-route'));
 
 module.exports = router;
