4be260a4522565053bc315c68980ecc7388011d3
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-08-02 12:34:49 -0300

    Adicionado category model.

--- backend/routers/api/v1/index.js
+++ backend/routers/api/v1/index.js
@@ -3,5 +3,5 @@ const router = require('express').Router();
 router.use('/user', require('./users-route'));
 router.use('/store', require('./store-route'));
 router.use('/customer', require('./customer-route'));
-
+router.use('/category', require('./category-route'));
 module.exports = router;
